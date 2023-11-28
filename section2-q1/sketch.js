0b3e5252e047ce47b030952bff47cd9c087b0fbc
Rinodesu <rinoooootanaka@gmail.com>
2023-11-28 13:41:26 +1100

    sec 2-q1 change

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(100,100)
+  noFill();
+ 
+ 
+  for(let i = 0; i <= 100;i=i+10){
     // BLANK[1]
+    ellipse(50,50,i,i)
+    if(i<=40){ stroke(0,0,255)}
+    else{stroke(255,0,0)};
+   
   }
 }
