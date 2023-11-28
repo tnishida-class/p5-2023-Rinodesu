863de37e1e70e36f6b5456d0b42a3ffc5abb3721
Rinodesu <rinoooootanaka@gmail.com>
2023-11-28 13:42:42 +1100

    sec4-q2 change

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,16 +1,16 @@
 // テキスト「配列を使った描画」練習問題：円グラフ
 
 function setup(){
-  createCanvas(400, 400);
-  background(240);
+  createCanvas(400, 400);//ベースの四角形
+  background(240);//ベースが灰色
 
   // 配列をランダムに初期化する
   let scores = [];
-  for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+  for(let i = 0; i < 10; i++){//360度を10分割したときの1つ分ずつ増やし続ける
+    scores[i] = random(20, 100); // 20以上100未満のランダムな数を代入
   }
 
-  // 円グラフを描くには割合が必要なので合計を計算しておく
+  // 円グラフを描くには割合が必要なので合計を計算しておく 553.4
   let total = 0;
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
 
